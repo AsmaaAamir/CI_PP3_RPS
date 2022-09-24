925043f6f840f32d91336ed73c44b54ff8b93e93
AsmaaAamir <asma.liaqat@outlook.com>
2022-09-24 04:12:12 +0000

    Add the main game variable to the run.py page

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def home_screen():
+    """
+    Welcome page with heading and main menu.
+    """
+    
+
+
+def game_rules():
+    """
+    Game rules - breifly explainging the user how to play the game.
+    how to win the game and how the user can lose the game.
+    """
+    pass
+
+
+def game():
+    """
+    Code for the game to play against the computer.
+    """
+    pass
+
+
+home_screen()
+game_rules()
+game()
