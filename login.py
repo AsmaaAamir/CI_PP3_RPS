d039332afaf546aa9d09cf8a521b4c66f180aa52
AsmaaAamir <asma.liaqat@outlook.com>
2022-09-24 05:46:56 +0000

    Added basic struture to login page

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Scope from Code institue Love Sandwiches project
+# Scope from Code Istituet Love Sandwiches walk-through project
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,5 +12,31 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('login_details')
+SHEET = GSPREAD_CLIENT.open('login_details').sheet1
+
+def login_menu():
+    pass
+
+
+def register():
+    pass
+
+
+def validate_register():
+    pass
+
+
+def update_login_worksheet():
+    pass
+
+
+def existing_players():
+    pass
+
+
+login_menu()
+register()
+validate_register()
+update_login_worksheet()
+existing_players()
 
