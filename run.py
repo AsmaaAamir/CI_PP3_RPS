384ef7a48b31f3de0741643a6ce874371ad40a34
AsmaaAamir <asma.liaqat@outlook.com>
2022-09-24 04:35:48 +0000

    Adding the rules to the game_rules variable

--- run.py
+++ run.py
@@ -28,20 +28,27 @@ def home_screen():
             print("Please enter your choice: \t")
             home_screen()
 
+
 def game_rules():
     """
     Game rules - breifly explainging the user how to play the game.
     how to win the game and how the user can lose the game.
     """
-    pass
+    print("------------------  Game Rules  -----------------------")
+    print(" ")
+    print("The rules are very simple:- ")
+    print(" ")
+    print("1. Player would be playing againt the computer.")
+    print("2. You can choose between Rock, Paper and Siccsors.")
+    print("3. You type in your choose then it the computer display it choice.\n")
+    print(" Ways your can *** WIN ***")
 
 
 def game():
     """
     Code for the game to play against the computer.
     """
-    pass
-
+    pass   
 
 home_screen()
 game_rules()
