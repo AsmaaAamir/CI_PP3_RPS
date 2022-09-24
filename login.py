3f911a864d5d1ad5c2fd5a35dfe98ea7f0562d82
AsmaaAamir <asma.liaqat@outlook.com>
2022-09-24 05:54:41 +0000

    Add doc string to variable on the login.py

--- login.py
+++ login.py
@@ -14,23 +14,44 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('login_details').sheet1
 
+
 def login_menu():
+    """
+    Gives the user the option to login or register.
+    """
     pass
 
 
 def register():
+    """
+    Allows user to enter their details for registeration.
+    """
     pass
 
 
 def validate_register():
+    """
+    Raises any errors in Username and passwords if they meet the rules,
+    stated in the register varibale.
+    """
     pass
 
 
 def update_login_worksheet():
+    """
+    Update the login sheet with new players details, their username, password
+    and email addess.
+    """
     pass
 
 
 def existing_players():
+    """
+    Allowing returing player to login to play the game.
+    Player will need to use there username and password to login.
+    The usernae is retrieved from the first column in login sheet.
+    The password is retrieved from the second column from login details.
+    """
     pass
 
 
@@ -39,4 +60,3 @@ register()
 validate_register()
 update_login_worksheet()
 existing_players()
-
